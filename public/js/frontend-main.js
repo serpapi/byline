df64ead8d830d765a88e8111ce6d1ee8ae6cc2c2
corbindavenport <corbindavenport@outlook.com>
2026-06-17 16:07:00 -0400

    Automatically save settings in web UI

--- public/js/frontend-main.js
+++ public/js/frontend-main.js
@@ -1,5 +1,20 @@
 const hostname = document.location.origin;
 
+// Automatically save all text field values to localStorage
+document.querySelectorAll("input[type='text']").forEach(function () {
+    this.addEventListener("change", function (event) {
+        localStorage.setItem(event.target.id, event.target.value);
+    })
+})
+
+// Restore settings from localStorage on page load
+document.addEventListener("DOMContentLoaded", function () {
+    for (const [key, value] of Object.entries(localStorage)) {
+        document.getElementById(key).value = value;
+    }
+})
+
+// Start button
 document.getElementById("byline-start-btn").addEventListener("click", async function () {
     // Create request for server
     const params = new URLSearchParams({
